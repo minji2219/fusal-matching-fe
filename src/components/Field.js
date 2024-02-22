f1a4467959393d4f045a9398003fbda84d0ec917
minji2219 <200146@jnu.ac.kr>
2024-02-22 16:16:29 +0900

    reserve페이지 수정

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,14 +1,68 @@
 import React, { useState } from "react";
-import img from "../image/Group 84.png";
 import "../css/components/Field.css";
 import Modal from "./Modal";
 import profile from "../image/ball.png";
 import { changeText } from "../helper/ChangeText";
 
-const Field = ({ field, endTime, startTime, date, fetchField }) => {
+const MatchingTeam = ({ team }) => {
+  const [hover, setHover] = useState("none");
+
+  return (
+    <div
+      className="matching__team"
+      onMouseOver={() => setHover("block")}
+      onMouseOut={() => setHover("none")}
+    >
+      <img
+        className="matcing__team--profile"
+        src={team.imageUrl ? team.imageUrl : profile}
+        alt="team__profile"
+      />
+      <div className="matcing__team--info" style={{ display: hover }}>
+        <div>팀명 : {team.teamName}</div>
+        <div>실력 : {changeText(team.skill)}</div>
+        <div>매너 : {changeText(team.manner)}</div>
+      </div>
+    </div>
+  );
+};
+
+const Field = ({ field, fetchField }) => {
   const [reserveSort, setReserveSort] = useState();
   const [modalState, setModalState] = useState(false);
-  const [hover, setHover] = useState("none");
+
+  const fieldOption = () => {
+    if (field.team) {
+      if (field?.team[0] && field?.team[1]) {
+        return (
+          <>
+            <MatchingTeam team={field?.team[0]} />
+            <MatchingTeam team={field?.team[1]} />
+          </>
+        );
+      } else {
+        return (
+          <>
+            <MatchingTeam team={field?.team[0]} />
+            <button className="matching__btn" onClick={MatchingReserve}>
+              매칭 신청
+            </button>
+          </>
+        );
+      }
+    } else {
+      return (
+        <>
+          <button className="matching__btn" onClick={MatchingReserve}>
+            매칭 신청
+          </button>
+          <button className="matching__btn" onClick={MatchingReserve}>
+            매칭 신청
+          </button>
+        </>
+      );
+    }
+  };
 
   const MatchingReserve = () => {
     setReserveSort("매칭 신청");
@@ -21,110 +75,16 @@ const Field = ({ field, endTime, startTime, date, fetchField }) => {
 
   return (
     <div className="field">
-      <div className="field_name">{field.fieldNum}구장</div>
-      <div className="field_background">
-        <div className="field_main">
-          <img src={img} alt="field" className="field_img" />
-
-          {field.team ? (
-            field.team[1] ? (
-              // 필드에 두 팀 모두 예약일때
-              <>
-                <div
-                  className="matching_team"
-                  onMouseOver={() => setHover("block")}
-                  onMouseOut={() => setHover("none")}
-                >
-                  <img
-                    src={
-                      field.team[0].imageUrl ? field.team[0].imageUrl : profile
-                    }
-                    style={{ width: "80px", height: "80px", marginTop: "15px" }}
-                    alt="profile"
-                  />
-                  <div className="teaminfo" style={{ display: hover }}>
-                    <div>팀명 : {field.team[0].teamName}</div>
-                    <div>실력 : {changeText(field.team[0].skill)}</div>
-                    <div>매너 : {changeText(field.team[0].manner)}</div>
-                  </div>
-                </div>
-                <div
-                  className="matching_team"
-                  style={{ left: "500px" }}
-                  onMouseOver={() => setHover("block")}
-                  onMouseOut={() => setHover("none")}
-                >
-                  <img
-                    src={
-                      field.team[1].imageUrl ? field.team[1].imageUrl : profile
-                    }
-                    style={{ width: "80px", height: "80px", marginTop: "15px" }}
-                    alt="profile"
-                  />
-                  <div className="teaminfo" style={{ display: hover }}>
-                    <div>팀명 : {field.team[1].teamName}</div>
-                    <div>실력 : {changeText(field.team[1].skill)}</div>
-                    <div>매너 : {changeText(field.team[1].manner)}</div>
-                  </div>
-                </div>
-              </>
-            ) : (
-              // 필드에 한팀만 예약일때
-              <>
-                <div
-                  className="matching_team"
-                  onMouseOver={() => setHover("block")}
-                  onMouseOut={() => setHover("none")}
-                >
-                  <img
-                    src={
-                      field.team[0].imageUrl ? field.team[0].imageUrl : profile
-                    }
-                    style={{ width: "80px", height: "80px", marginTop: "15px" }}
-                    alt="profile"
-                  />
-                  <div className="teaminfo" style={{ display: hover }}>
-                    <div>팀명 : {field.team[0].teamName}</div>
-                    <div>실력 : {changeText(field.team[0].skill)}</div>
-                    <div>매너 : {changeText(field.team[0].manner)}</div>
-                  </div>
-                </div>
-                {field.allRental ? (
-                  <></>
-                ) : (
-                  <button className="matching_btn" onClick={MatchingReserve}>
-                    매칭 신청
-                  </button>
-                )}
-              </>
-            )
-          ) : (
-            // 필드에 아무 예약도 없을때
-            <>
-              <button
-                className="matching_btn"
-                style={{ top: "160px", left: "145px" }}
-                onClick={MatchingReserve}
-              >
-                매칭 신청
-              </button>
-              <button className="matching_btn" onClick={MatchingReserve}>
-                매칭 신청
-              </button>
-            </>
-          )}
-        </div>
+      <div className="field__number">{field.fieldNum}구장</div>
+      <div className="field__background">
+        <div className="field__img">{fieldOption()}</div>
       </div>
       {field.team ? (
-        <>
-          {field.allRental ? (
-            <div className="complete">예약이 완료된 구장입니다.</div>
-          ) : (
-            <></>
-          )}
-        </>
+        field.allRental && (
+          <div className="reserve--complete">* 예약이 완료된 구장입니다.</div>
+        )
       ) : (
-        <button className="whole_btn" onClick={WholdReserve}>
+        <button className="whole--reserve__btn" onClick={WholdReserve}>
           전체 대여
         </button>
       )}
@@ -132,14 +92,15 @@ const Field = ({ field, endTime, startTime, date, fetchField }) => {
       {modalState ? (
         <>
           <Modal
+            title="이 일정으로 예약할까요?"
             fetchField={fetchField}
             field={field}
             setModalState={setModalState}
             reserveSort={reserveSort}
             fieldName={field.fieldNum}
-            date={date}
-            startTime={startTime}
-            endTime={endTime}
+            date={field.matchingDate}
+            startTime={field.startTime}
+            endTime={field.endTime}
           />
           <div
             style={{
--- src/pages/ReservePage.js
+++ src/pages/ReservePage.js
@@ -1,17 +1,17 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import "../css/pages/reservePage.css";
 import Field from "../components/Field";
 import { apiGet } from "../helper/api";
+import "../css/pages/reservePage.css";
 
 const ReservePage = () => {
   const location = useLocation();
-  const id = location.state?.id;
+  const stadiumId = location.state?.id;
 
   const [stadiumList, setStadiumList] = useState(null);
-  const [stadiumValue, setStadiumValue] = useState(id ? id : 1);
+  const [stadiumValue, setStadiumValue] = useState(stadiumId ? stadiumId : 1);
   const [dateValue, setDateValue] = useState();
+  const [fields, setFields] = useState(null);
 
   let hour = 0;
   let minute = 0;
@@ -28,6 +28,7 @@ const ReservePage = () => {
     }`;
     times.push(time);
   }
+
   const [startTimeValue, setStartTimeValue] = useState(times[0]);
   const [endTimeValue, setEndTimeValue] = useState(times[0]);
 
@@ -54,33 +55,11 @@ const ReservePage = () => {
     settingStadiums();
   }, []);
 
-  const [field, setField] = useState(null);
-
   const fetchField = async () => {
-    try {
-      const result = await axios.get(
-        `https://6f2b-121-147-100-85.ngrok-free.app/stadiums/fields?id=${stadiumValue}&date=${dateValue}&time=${startTimeValue}:00`,
-        {
-          headers: {
-            "Content-Type": `application/json`,
-            "ngrok-skip-browser-warning": "69420",
-          },
-        }
-      );
-      setField(result.data);
-    } catch (err) {
-      console.log("err입니당~", err);
-    }
-  };
-
-  const changeDateValue = (e) => {
-    setDateValue(e.target.options[e.target.selectedIndex].text);
-  };
-  const changeStartTimeValue = (e) => {
-    setStartTimeValue(e.target.options[e.target.selectedIndex].text);
-  };
-  const changeEndTimeValue = (e) => {
-    setEndTimeValue(e.target.options[e.target.selectedIndex].text);
+    const data = await apiGet(
+      `stadiums/fields?id=${stadiumValue}&date=${dateValue}&time=${startTimeValue}:00`
+    );
+    setFields(data);
   };
 
   return (
@@ -99,11 +78,13 @@ const ReservePage = () => {
             </span>
             <select
               style={{ fontSize: "16px" }}
-              value={id}
+              value={stadiumValue}
               onChange={(e) => setStadiumValue(e.target.value)}
             >
               {stadiumList?.map((stadium) => (
-                <option value={stadium.id}>{stadium.stadiumName}</option>
+                <option key={stadium.id} value={stadium.id}>
+                  {stadium.stadiumName}
+                </option>
               ))}
             </select>
           </div>
@@ -115,15 +96,19 @@ const ReservePage = () => {
             >
               calendar_month
             </span>
-            <select style={{ fontSize: "16px" }} onChange={changeDateValue}>
+            <select
+              style={{ fontSize: "16px" }}
+              value={dateValue}
+              onChange={(e) => setDateValue(e.target.value)}
+            >
               {settingDates()?.map((date) => (
-                <option>
+                <option key={date}>
                   {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}
                 </option>
               ))}
             </select>
           </div>
-
+          {/* TODO:날짜 조회 형식 바뀌면 바꾸기 */}
           <div style={{ display: "flex", alignItems: "center" }}>
             <span
               style={{ fontSize: "30px", marginRight: "10px" }}
@@ -132,33 +117,30 @@ const ReservePage = () => {
               history
             </span>
             <select
-              onChange={changeStartTimeValue}
+              onChange={(e) => {
+                setStartTimeValue(e.target.value);
+              }}
+              value={startTimeValue}
               style={{ fontSize: "16px" }}
             >
               {times?.map((time) => (
-                <option>{time}</option>
+                <option key={time}>{time}</option>
               ))}
             </select>
-            <div style={{ margin: "10px", fontSize: "16px" }}>~</div>
+            {/* <div style={{ margin: "10px", fontSize: "16px" }}>~</div>
             <select onChange={changeEndTimeValue} style={{ fontSize: "16px" }}>
               {times?.map((time) => (
                 <option>{time}</option>
               ))}
-            </select>
+            </select> */}
           </div>
           <button onClick={fetchField}>조회</button>
         </div>
         <hr />
 
-        {field?.map((field) => (
-          <Field
-            fetchField={fetchField}
-            field={field}
-            date={dateValue}
-            startTime={startTimeValue}
-            endTime={endTimeValue}
-          />
-        ))}
+        {fields?.map(
+          (field) => field && <Field fetchField={fetchField} field={field} />
+        )}
       </div>
     </>
   );
