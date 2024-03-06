8b5897118d4058e595ceaf5b0f2a140d630ed7cd
minji2219 <200146@jnu.ac.kr>
2024-03-07 00:16:07 +0900

    프로필 수정시 동작 추가
    프로필 이미지 수정시 미리보기 띄우기

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -9,6 +9,10 @@ import MatchingHistory from "../components/MatchingHistory";
 const MyPage = () => {
   const userId = localStorage.getItem("userId");
   const [team, setTeam] = useState();
+  const [editDisabled, setEditDisabled] = useState(true);
+  const [saveBtnDisplay, setSaveBtnDisplay] = useState("hidden");
+  const [postImg, setPostImg] = useState();
+  const [previewImage, setPreviewImage] = useState();
 
   const teamFetch = async () => {
     const data = await apiGet(`teams?id=${userId}`);
@@ -17,58 +21,117 @@ const MyPage = () => {
   useEffect(() => {
     teamFetch();
   }, []);
-
+  const handleEdit = () => {
+    setEditDisabled(false);
+    setSaveBtnDisplay("visible");
+  };
+  const handleUpdate = () => {
+    //TODO:데이터 수정 서버 생성시 API 연결
+    setEditDisabled(true);
+    setSaveBtnDisplay("hidden");
+  };
+  const uploadFile = (e) => {
+    let file = e.target?.files[0];
+    setPostImg(file);
+    let fileRead = new FileReader();
+    fileRead.onload = () => {
+      setPreviewImage(fileRead.result);
+    };
+    fileRead.readAsDataURL(file);
+  };
   return (
-    <div>
+    <div className="mypage__whole">
       <div className="center mypage">
         <div className="mypage__teaminfo">
           <div className="mypage__icons">
-            <span className="material-symbols-outlined">lock</span>{" "}
-            <span className="material-symbols-outlined">edit</span>
+            <span
+              className="mypage__icon material-symbols-outlined"
+              style={{ visibility: saveBtnDisplay }}
+              onClick={handleUpdate}
+            >
+              save
+            </span>
+            <span
+              className="mypage__icon material-symbols-outlined"
+              onClick={handleEdit}
+            >
+              edit
+            </span>
           </div>
           <div className="mypage__teamintro">
             <div className="team__profile">
-              <img
-                src={team?.imageUrl ? team.imageUrl : profile}
-                style={{ width: "100px", height: "100px" }}
-                alt="profile"
-              />
+              {editDisabled ? (
+                <img
+                  src={team?.imageUrl ? team.imageUrl : profile}
+                  width={100}
+                  height={100}
+                  alt="profile"
+                />
+              ) : (
+                <div className="profile">
+                  {previewImage ? (
+                    <img
+                      alt={previewImage}
+                      src={previewImage}
+                      width={80}
+                      height={80}
+                    />
+                  ) : (
+                    <label htmlFor="input-file" className="profile__upload">
+                      <span className="material-symbols-outlined">upload</span>
+                    </label>
+                  )}
+                  <input
+                    type="file"
+                    id="input-file"
+                    accept="image/*"
+                    onChange={uploadFile}
+                  />
+                </div>
+              )}
             </div>
             <div className="team__contents">
               <div className="team__content">
-                <span className="team__key">팀명</span>
-                {team?.teamName}
+                <div className="team__key">팀명</div>
+                <input
+                  className="team__value"
+                  placeholder={team?.teamName}
+                  disabled={editDisabled}
+                />
               </div>
               <div className="team__content">
-                <span className="team__key">주장</span>
-                {team?.captainName}
+                <div className="team__key">주장</div>
+                <input
+                  className="team__value"
+                  placeholder={team?.captainName}
+                  disabled={editDisabled}
+                />
               </div>
               <div className="team__content">
-                <span className="team__key">전화번호</span>
-                {team?.tel}
+                <div className="team__key">전화번호</div>
+                <input
+                  className="team__value"
+                  placeholder={team?.tel}
+                  disabled={editDisabled}
+                />
               </div>
               <div className="team__content">
-                <span className="team__key">실력</span>
-                {changeText(team?.skill)}
+                <div className="team__key">실력</div>
+                <input
+                  className="team__value"
+                  placeholder={changeText(team?.skill)}
+                  disabled
+                />
               </div>
               <div className="team__content">
-                <span className="team__key">매너</span>
-                {changeText(team?.manner)}{" "}
+                <div className="team__key">매너</div>
+                <input
+                  className="team__value"
+                  placeholder={changeText(team?.manner)}
+                  disabled
+                />
               </div>
             </div>
-          </div>
-          <div className="team__awards">
-            <div className="mypage__icon">
-              <span className="material-symbols-outlined">rewarded_ads</span>
-              수상 경력
-            </div>
-            <hr />
-            {/* TODO:경력 데이터 받아오기 */}
-            <ul className="team__awards__list">
-              <li>2019년도 '오로라' 풋살 대회 준우승</li>
-              <li>2019년도 '오로라' 풋살 대회 준우승</li>
-              <li>2019년도 '오로라' 풋살 대회 준우승</li>
-            </ul>
           </div>
         </div>
         <MatchingHistory
